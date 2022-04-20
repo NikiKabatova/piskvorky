86c34ce51ab867ab3cc7a0f2e16740e08958e696
Nikola Kabátová <niki.kabatova@gmail.com>
2022-04-20 08:24:22 +0200

    eventCircle+styles background- image

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Funguju!');
+const itsYourTurn = 'circle';
+
+const move = (event) => {
+  event.target.classList.add('board__field--circle');
+};
+
+const buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', move);
+}
