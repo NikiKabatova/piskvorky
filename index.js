98d3d482fa31f475475c9f938197f8d2847d3894
Nikola Kabátová <niki.kabatova@gmail.com>
2022-04-22 08:14:32 +0200

    if/ else

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-console.log('Funguju!');
-
 let itsYourTurn = 'circle';
+let currentPlayer = document.getElementById('currentPlayer');
 
 const move = (event) => {
-  //if (itsYourTurn === 'circle') {
-  event.target.classList.add('board__field--circle');
-  //} else {
-  //  event.target.classList.add('board__field--cross');
-  //}
+  if (itsYourTurn === 'circle') {
+    event.target.classList.add('board__field--circle');
+    itsYourTurn = 'cross';
+  } else {
+    itsYourTurn = 'circle';
+    event.target.classList.add('board__field--cross');
+  }
   console.log(event);
 };
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.game__field button');
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', move);
 }
